6bb93ea9790d20dce7e31dbfe8ea1d2e0dafd3c7
Caesar Isidro Va-ay <caesarisidrovaay@gmail.com>
2024-12-28 21:24:52 +0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-10">
+      <h2>Vulos Ecommerce website</h2>
+      <Button>Demo button</Button>
+    </div>
+  );
+}
