fd48ea8d09ff15f3b9ce124c90a4f7207c84c661
Caesar Isidro Va-ay <caesarisidrovaay@gmail.com>
2024-12-30 21:53:41 +0800

    add authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import {ClerkProvider} from '@clerk/nextjs'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+          <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -34,5 +36,6 @@ export default function RootLayout({
         <Footer />
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -3,7 +3,7 @@ import Title from './Title'
 
 const HomeBanner = () => {
   return (
-    <div className='flex flex-col items-center justify-center gap-5'>
+    <div className='flex flex-col items-center justify-center gap-5npm install @clerk/nextjs'>
       <Title className="text-3xl md:text-4xl uppercase font-bold text-center">Best Clothing Collection</Title>
       <p className='text-sm text-center text-lightColor/80 font-medium max-w-[480px]'>Find everything you need to look and feel your best, and shop the latest men&apos;s fashion and lifestyle products</p>
     </div>
