7e61112419185f2698fb48bebe8ba324aa5c08a5
Caesar Isidro Va-ay <caesarisidrovaay@gmail.com>
2025-01-01 21:23:45 +0800

    finish sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,9 @@ import { X } from 'lucide-react';
 import { headerData } from '@/constants';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import { on } from 'events';
+import SocialMedia from './SocialMedia';
+import { useOutsideClick } from '@/hooks/useOutsideClick';
 
 interface SidebarProps{
     isOpen:boolean;
@@ -13,13 +16,18 @@ interface SidebarProps{
 
 const Sidebar:FC<SidebarProps> = ({isOpen, onClose}) => {
   const pathname= usePathname();
+  const sidebarRef = useOutsideClick<HTMLDivElement>(onClose);
   return (
-    <div className={`fixed inset-y-0 left-0 z-50 bg-darkColor/50 shadow-xl hoverEffect w-full ${
+    <div className={`fixed inset-y-0 left-0 z-50 bg-darkColor/50 shadow-xl hoverEffect cursor-auto w-full ${
       isOpen ? 'translate-x-0': "-translate-x-full"}`}>
-      <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration:0.4, delay: 0.3}} 
+      <motion.div initial={{opacity:0}} animate={{opacity:1}} 
+      transition={{duration:0.4, delay: 0.3}} 
+      ref={sidebarRef}
       className='min-w-72 max-w-96 bg-darkColor text-white/70 h-full p-10 border-r border-r-white flex flex-col gap-6'>
         <div className='flex items-center justify-between'>
-          <Logo className='text-white'>Vulos</Logo>
+          <button onClick={onClose}>
+            <Logo className='text-white'>Vulos</Logo>
+          </button>
           <button className='hover:text-red-500 hoverEffect' onClick = {onClose}>
             <X className='w-7 h-7'/>
           </button>
@@ -27,9 +35,10 @@ const Sidebar:FC<SidebarProps> = ({isOpen, onClose}) => {
         <div className='flex flex-col gap-3.5 text-base font-semibold tracking-wide'>
       {headerData?.map((item)=>(
         <Link 
+        onClick={onClose}
         key={item?.title} 
         href={item?.href}
-        className={`hover:text-white hoverEffect ${
+        className={`hover:text-white hoverEffect w-24 ${
             pathname === item?.href && "text-whiter"
         }`}
         >
@@ -38,6 +47,7 @@ const Sidebar:FC<SidebarProps> = ({isOpen, onClose}) => {
         </Link>
       ))}
     </div>
+    <SocialMedia />
       </motion.div>
     </div>
   )
