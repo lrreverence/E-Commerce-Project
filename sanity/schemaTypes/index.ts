28b75db11e92e8db242020ac48bc6251ee90e996
Caesar Isidro Va-ay <caesarisidrovaay@gmail.com>
2025-01-02 21:13:28 +0800

    use sanity for backend

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,7 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import {categoryType} from './categoryType'
+import { productType } from './productType'
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [categoryType, productType],
+}
