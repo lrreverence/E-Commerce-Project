fda814cace65f9ae6f33dd1ff934be22a1a58b72
Caesar Isidro Va-ay <caesarisidrovaay@gmail.com>
2025-01-08 22:10:33 +0800

    add disable function if a product is out of stock

---
+++ components/AddToCartButton.tsx
@@ -0,0 +1,21 @@
+import { Product } from '@/sanity.types'
+import React from 'react'
+import { Button } from './ui/button';
+import { cn } from '@/lib/utils';
+
+interface Props {
+    product: Product;
+    className?: string;
+}
+
+const AddToCartButton = ({product, className}: Props) => {
+    const isOutOfStock = product?.stock === 0;
+  return (
+    <div>
+      <Button disabled={isOutOfStock} className={
+        cn("w-full bg-transparent text-darkColor shadow-none border border-darkColor/30font-semibold tracking-wide hover:text-white hoverEffect",className)}>Add to cart</Button>
+    </div>
+  )
+}
+
+export default AddToCartButton
