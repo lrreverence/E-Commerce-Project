2258bf6ebb6a749f6b811102751397d6cd9eb408
Caesar Isidro Va-ay <caesarisidrovaay@gmail.com>
2025-01-09 13:57:50 +0800

    add a new feature where you can add or reduce products

---
+++ app/(client)/product/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import Container from '@/components/Container'
+import React from 'react'
+
+const SingleProductPage = () => {
+  return (
+    <div>
+        <Container>singleproductpage</Container>
+    </div>
+  )
+}
+
+export default SingleProductPage
--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -2,6 +2,8 @@ import { Product } from '@/sanity.types'
 import React from 'react'
 import { Button } from './ui/button';
 import { cn } from '@/lib/utils';
+import QuantityButtons from './QuantityButtons';
+import PriceFormatter from './PriceFormatter';
 
 interface Props {
     product: Product;
@@ -10,10 +12,22 @@ interface Props {
 
 const AddToCartButton = ({product, className}: Props) => {
     const isOutOfStock = product?.stock === 0;
+    const itemCount = 0;
   return (
     <div>
-      <Button disabled={isOutOfStock} className={
-        cn("w-full bg-transparent text-darkColor shadow-none border border-darkColor/30font-semibold tracking-wide hover:text-white hoverEffect",className)}>Add to cart</Button>
+      {itemCount ? 
+      <div className='w-full text-sm'>
+        <div className='flex items-center justify-between'>
+          <span className='text-xs text-muted-foreground'>Quantity</span>
+          <QuantityButtons product={product}/>
+        </div>
+        <div className='flex items-center justify-between border-t pt-1'>
+          <span className='font-semibold text-xs'>Subtotal</span>
+          <PriceFormatter amount={product?.price ? product ?.price *itemCount : 0}/>
+        </div>
+      </div> :
+       <Button disabled={isOutOfStock} className={
+        cn("w-full bg-transparent text-darkColor shadow-none border border-darkColor/30font-semibold tracking-wide hover:text-white hoverEffect",className)}>Add to cart</Button>}
     </div>
   )
 }
