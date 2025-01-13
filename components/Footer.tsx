bf7fc70504a98c62b92d95c2cc5570b69e025f4f
Caesar Isidro Va-ay <caesarisidrovaay@gmail.com>
2025-01-13 21:28:04 +0800

    Add social media account in the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import Container from './Container'
 import FooterTop from './FooterTop'
+import Logo from './Logo'
+import SocialMedia from './SocialMedia'
+import { Input } from './ui/input'
 
 const Footer = () => {
   return (
     <footer className='bg-white border-t'>
         <Container>
           <FooterTop />
-            Footer
+          <div className='py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8'>
+            <div className='space-y-4'>
+              <Logo>Vulos</Logo>
+              <p className='text-gray-600 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, incidunt blanditiis.</p>
+              <SocialMedia className='text-darkColor/60' iconClassName='border-darkColor/60 hover:border-darkColor hover:text-darkColor' tooltipClassName='bg-darkColor text-white'/>
+            </div>
+            <div></div>
+            <div></div>
+            <div>
+              <h3>Newsletter</h3>
+              <p>Subscribe to our newsletter to recieve updates and exclusive offers.</p>
+              <form>
+                <Input type='email' placeholder='Enter your email' required className=''/>
+                <button type='submit'>Subscribe</button>
+              </form>
+            </div>
+          </div>
         </Container>
     </footer>
   )
