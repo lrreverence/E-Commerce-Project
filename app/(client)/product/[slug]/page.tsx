7544cd01d16c475735e38bed85f5e4adaf8913f1
Caesar Isidro Va-ay <caesarisidrovaay@gmail.com>
2025-01-21 21:50:06 +0800

    add stock status

--- app/(client)/product/[slug]/page.tsx
+++ app/(client)/product/[slug]/page.tsx
@@ -22,6 +22,9 @@ const SingleProductPage = async({params}:{
               <h2>{product?.name}</h2>
               <PriceView price={product?.price} discount={product?.discount} className='text-lg font-bold'/>
             </div>
+            {product?.stock && (<p className='bg-green-100 w-24 text-center text-green-600 text-sm py-2.5 font-semibold rounded-lg'>In Stock</p>
+          )}
+          <p className='text-sm text-gray-600 tracking-wide'>{product?.description}</p>
           </div>
         </Container>
   )
