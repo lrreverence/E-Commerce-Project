f1ce991214e13ec0d7ed067394be7385e35ee9a0
Caesar Isidro Va-ay <caesarisidrovaay@gmail.com>
2025-01-30 16:05:44 +0800

    add the product details when searched

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,15 @@
 "use client"
-import { Search, X } from 'lucide-react'
+import { Loader2, Search, X } from 'lucide-react'
 import React, { useCallback, useEffect, useState } from 'react'
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog'
 import { Input } from './ui/input'
 import { client } from '@/sanity/lib/client'
+import Link from 'next/link'
+import { Product } from '@/sanity.types'
+import Image from 'next/image'
+import { urlFor } from '@/sanity/lib/image'
+import PriceView from './PriceView'
+import AddToCartButton from './AddToCartButton'
 
 const SearchBar = () => {
   const [search, setSearch] = useState("");
@@ -39,7 +45,7 @@ const SearchBar = () => {
       <DialogTrigger onClick={() => setShowSearch(!showSearch)}>
         <Search className='w-5 h-5 hover:text-darkColor hoverEffect'/>
       </DialogTrigger>
-      <DialogContent className='max-w-5xl h-[90vh] flex flex-col overflow-hidden'>
+      <DialogContent className='max-w-5xl min-h-[90vh] max-h-[90vh] flex flex-col overflow-hidden'>
         <DialogHeader>
           <DialogTitle className='mb-1'>
             Product Searchbar
@@ -57,8 +63,31 @@ const SearchBar = () => {
         </DialogHeader>
         <div className='w-full h-full overflow-y-scroll border border-darkColor/20 rounded-md '>
           <div>
-            {loading ? <p>Searching in progress...</p>:products.length ? <div>Products available</div>:<div className='text-center py-10 font-semibold tracking-wide'>
-              {search && products?.length ? <p>No match</p>:<p className='text-green-600 flex items-center justify-center gap-1'><Search className='w-5 h-5'/>Search and explore your products from Vulos.</p>}
+            {loading ? <p className='flex items-center px-6 py-10 gap-1 text-center text-yellow-600 font-semibold'>
+              <Loader2 className='h-5 w-5 animate-spin'/>
+            Searching in progress...</p>:products.length ? (
+            products?.map((product:Product)=>(
+              <div key={product?._id} className='bg-white overflow-hidden border-b last:border-b-0'>
+                <div className='flex items-center p-1'>
+                <Link href={`/product/${product?.slug?.current}`} className='h-20 w-20 md:h-24 md:w-24 flex-shrink-0 border border-darkColor/20 rounded-md overflow-hidden group' onClick={()=> setShowSearch(false)}>
+                  {product?.images && (<Image width={200} height={200} src={urlFor(product?.images[0]).url()} alt='productImage' 
+                  className='object-cover w-full h-full group-hover:scale-110 hoverEffect'/>)}
+                </Link>
+                <div className='px-4 py-2 flex-grow'>
+                  <Link href={`/product/${product?.slug?.current}`} onClick={()=> setShowSearch(false)}>
+                  <h3 className='text-sm md:text-lg font-semibold text-gray-800 line-clamp-1'>{product?.name}</h3>
+                  <p className='text-sm text-gray-600 line-clamp-1'>{product?.intro}</p>
+                  </Link>
+                  <PriceView price={product?.price} discount={product?.discount} className='md:text-lg' />
+                </div>
+                <div className='w-60 mt-1'>
+                  <AddToCartButton product={product}/>
+                </div>
+                </div>
+              </div>
+            ))
+          ):<div className='text-center py-10 font-semibold tracking-wide'>
+              {search ? <p>Nothing matches the keyword <span className='underline text-red-600'>{search}</span>. Please try something else.</p>:<p className='text-green-600 flex items-center justify-center gap-1'><Search className='w-5 h-5'/>Search and explore your products from Vulos.</p>}
               </div>}
           </div>
         </div>
