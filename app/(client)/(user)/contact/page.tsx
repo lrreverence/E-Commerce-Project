6aab84f4a0529a892eb5ef93291308fe0787bacf
Caesar Isidro Va-ay <caesarisidrovaay@gmail.com>
2025-02-01 21:28:57 +0800

    add description in the contacts page

--- app/(client)/(user)/contact/page.tsx
+++ app/(client)/(user)/contact/page.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 
 const ContactPage = () => {
   return (
-    <Container>contact page</Container>
+    <Container className='max-w-3xl px-4 sm:px-6 lg:px-8 py-12'>
+      <h1>Contact Us</h1>
+      <p className='mb-6'>
+        We&apos;d love to hear from you. Please fill out the form below and we&apos;ll get back to you as soon as possible.
+      </p>
+    </Container>
   )
 }
 
